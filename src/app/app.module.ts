d7a2447ed22b5f5efeb9a29dc34c9129dd0f4c94
hamoghamdi <haneen.aljabrah@gmail.com>
2021-08-04 18:13:43 +0300

    feat: api, GET requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http'
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
+
+import {InMemoryDataService } from './services/in-memory-data.service'
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
@@ -18,8 +23,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
-    BrowserModule, FormsModule, AppRoutingModule,
-  ],
+    BrowserModule, FormsModule, AppRoutingModule, HttpClient, HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false})
+  ], // The HttpClientInMemoryWebApiModule module intercepts HTTP requests and returns simulated server responses.
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -3,22 +3,54 @@ import { HEROES } from '../mock-heroes'
 import { Hero } from '../hero'
 import { Observable, of } from 'rxjs'
 import { MessageService } from './message.service'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { catchError, map, tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private mssgservice: MessageService) { } // "service-in-service" 
+  private heroesUrl = 'api/heroes'
+  
+  constructor(private mssgservice: MessageService, private myhttp: HttpClient) { } // "service-in-service" 
+
+  private log(mssg: string){
+    this.mssgservice.add(`HeroService: ${mssg}`)
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation: string = 'operation', result?:T){
+    return (error: any): Observable<T> => {
+      console.log(error) // send the error to remote logging infrastructure
+
+      this.log(`${operation} failed: ${error.message}`) // better job of transforming error for user consumption
+
+      return of(result as T) // empty result 
+    }
+  }
 
   getHeros() : Observable<Hero[]>{
-    const heroes = of(HEROES);
-    this.mssgservice.add("Hero Service: fetched Heroes");
-    return heroes;
+    // const heroes = of(HEROES);
+    this.log("Hero Service: fetched Heroes");
+    return this.myhttp.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    )
   }
   getHero(id:number): Observable<Hero>{
-    const hero : Hero = HEROES.find(hero => hero.id === id)!;
-    this.mssgservice.add(`HeroService: fetched hero with id ${id}`)
-    return of(hero)
+    // const hero : Hero = HEROES.find(hero => hero.id === id)!;
+    const url = `api/heroes/${id}`
+    this.log(`HeroService: fetched hero with id ${id}`)
+    return this.myhttp.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero with id ${id}`)), catchError(this.handleError<Hero>(`getHero id ${id}`))
+    )
   }
+
+
 }

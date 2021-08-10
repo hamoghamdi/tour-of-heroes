76c4ff2031ee8959a5aa99eac5cf94989d7a6527
hamoghamdi <haneen.aljabrah@gmail.com>
2021-08-10 09:47:26 +0300

    fix: refactor pipe code lines

--- src/app/wave.pipe.ts
+++ src/app/wave.pipe.ts
@@ -5,15 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class WavePipe implements PipeTransform {
         transform(value:string): string{
-            let updatedValue = [];
-            for(let i=0; i<value.length; i++){
-                if(i%2==0) {
-                    updatedValue.push(value[i].toUpperCase());
-                } else { 
-                    updatedValue.push(value[i].toLowerCase());
-                }
-                
-            }
-            return updatedValue.join('')
+
+            return value.split("").map( (letter,index) => (
+                index%2==0 ? letter.toUpperCase() : letter.toLowerCase()
+            )).join('')
+
         }
 }
